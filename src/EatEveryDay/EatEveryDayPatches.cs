7e2d539dbf18ae4e702efc221af738efe0868039
SanchozzDeponianin <73459473+SanchozzDeponianin@users.noreply.github.com>
2021-08-13 10:08:58 +0600

    EatEveryDay: the version of mod, originally created by ricvail, updated to SpaceOut and Mergedown with the author's permission.
    It forces duplicants to eat every day, even at the "no sweat" difficulty level, which guarantees a stable morale bonus.
    https://steamcommunity.com/sharedfiles/filedetails/?id=2107702766

---
+++ src/EatEveryDay/EatEveryDayPatches.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+
+namespace EatEveryDay
+{
+	// обновленная версия мода "Eat every day Mod" от ricvail
+	// https://steamcommunity.com/sharedfiles/filedetails/?id=2107702766 
+	public sealed class EatEveryDayPatches : KMod.UserMod2
+	{
+		[HarmonyPatch(typeof(CalorieMonitor.Instance), nameof(CalorieMonitor.Instance.IsHungry))]
+		public static class CalorieMonitor_Instance_IsHungry
+		{
+			public static void Postfix(CalorieMonitor.Instance __instance, ref bool __result)
+			{
+				if (__instance.IsSatisfied())
+					return;
+				float delta = __instance.calories.GetDelta();
+				if (delta >= 0f)
+					return;
+				float consumed_per_cycle = delta * Constants.SECONDS_PER_CYCLE;
+				float thresold = __instance.calories.GetMax() + consumed_per_cycle * 0.75f;
+				__result = __instance.calories.value < thresold;
+			}
+		}
+	}
+}
