236a579437e65a95b51fe3a46fb030a0db416a03
SanchozzDeponianin <73459473+SanchozzDeponianin@users.noreply.github.com>
2021-12-04 15:46:26 +0600

    LargeTelescope: text update

--- src/LargeTelescope/STRINGS.cs
+++ src/LargeTelescope/STRINGS.cs
@@ -1,10 +1,13 @@
-﻿using STRINGS;
+﻿using System.Collections.Generic;
+using STRINGS;
 using static STRINGS.BUILDINGS.PREFABS;
+using SanchozzONIMods.Lib;
 
 namespace LargeTelescope
 {
     public class STRINGS
     {
+        private const string ENCLOSED_TELESCOPE = "{ENCLOSED_TELESCOPE}";
         public class BUILDINGS
         {
             public class PREFABS
@@ -23,22 +26,35 @@ namespace LargeTelescope
             public class ANALYZE_CLUSTER_RADIUS
             {
                 public static LocString NAME = "Scan Radius, hex";
-                public static LocString TOOLTIP = "A small Telescope has a radius = 3";
+                public static LocString TOOLTIP = "Unmodded radius = 4\nA small Telescope has a radius = 3";
             }
             public class EFFICIENCY_MULTIPLIER
             {
-                public static LocString NAME = "Workspeed modifier, +X%";
+                public static LocString NAME = "Scan Workspeed modifier, +X%";
+            }
+            public class ADD_GLASS
+            {
+                public static LocString NAME = $"Add Glass to the {ENCLOSED_TELESCOPE}`s construction cost";
+            }
+            public class NOT_REQUIRE_GAS_PIPE
+            {
+                public static LocString NAME = "Make the Gas Pipe connection requirement optional instead of mandatory";
+            }
+            public class PROHIBIT_INSIDE_ROCKET
+            {
+                public static LocString NAME = $"Prohibit the construction of the {ENCLOSED_TELESCOPE} inside a Rocket";
             }
             public class FIX_NO_CONSUME_POWER_BUG
             {
-                public static LocString NAME = "Fix the Bug";
-                public static LocString TOOLTIP = "that Telescopes don't actually consume Power";
+                public static LocString NAME = "Fix a bug where telescopes don't actually consume Power";
             }
         }
 
         internal static void DoReplacement()
         {
             BUILDINGS.PREFABS.CLUSTERLARGETELESCOPE.EFFECT = CLUSTERTELESCOPE.EFFECT + BUILDINGS.PREFABS.CLUSTERLARGETELESCOPE.EFFECT;
+            Utils.ReplaceAllLocStringTextByDictionary(typeof(STRINGS), new Dictionary<string, string>
+                { { ENCLOSED_TELESCOPE, CLUSTERTELESCOPEENCLOSED.NAME } });
             LocString.CreateLocStringKeys(typeof(BUILDINGS));
         }
     }
