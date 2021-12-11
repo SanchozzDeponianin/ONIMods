b1f79fd4c47823b070c906623901aa94119f11e2
SanchozzDeponianin <73459473+SanchozzDeponianin@users.noreply.github.com>
2021-12-11 11:02:19 +0600

    a new mod WornSuitDischarge that discharges Worn Suits so they don't drop the Canisters.
    Oxygen and Kerosene are returned to the Suit Dock for later reuse.

---
+++ src/WornSuitDischarge/DurabilityExtensions.cs
@@ -0,0 +1,36 @@
+﻿using TUNING;
+using UnityEngine;
+using PeterHan.PLib.Detours;
+
+namespace WornSuitDischarge
+{
+    public static class DurabilityExtensions
+    {
+        // оригинальные функции дают неправильное значение пока костюм надет
+        // там в формуле перепутан плюс на минус, а также не учитываются кофициенты сложности игры и навыка дупеля
+
+        public static IDetouredField<Durability, bool> isEquipped = PDetours.DetourFieldLazy<Durability, bool>("isEquipped");
+        public static IDetouredField<Durability, float> durability = PDetours.DetourFieldLazy<Durability, float>("durability");
+        public static IDetouredField<Durability, float> difficultySettingMod = PDetours.DetourFieldLazy<Durability, float>("difficultySettingMod");
+
+        public static float GetTrueDurability(this Durability @this, MinionResume resume = null)
+        {
+            if (isEquipped.Get(@this))
+            {
+                float delta = GameClock.Instance.GetTimeInCycles() - @this.TimeEquipped;
+                delta *= @this.durabilityLossPerCycle * difficultySettingMod.Get(@this);
+                if (resume != null && resume.HasPerk(Db.Get().SkillPerks.ExosuitDurability.Id))
+                {
+                    delta *= 1f - EQUIPMENT.SUITS.SUIT_DURABILITY_SKILL_BONUS;
+                }
+                return Mathf.Clamp01(durability.Get(@this) + delta);
+            }
+            return durability.Get(@this);
+        }
+
+        public static bool IsTrueWornOut(this Durability @this, MinionResume resume = null)
+        {
+            return @this.GetTrueDurability(resume) <= 0f;
+        }
+    }
+}
---
+++ src/WornSuitDischarge/WornSuitDischargePatches.cs
@@ -0,0 +1,41 @@
+﻿using HarmonyLib;
+using PeterHan.PLib.Core;
+
+namespace WornSuitDischarge
+{
+    internal sealed class WornSuitDischargePatches : KMod.UserMod2
+    {
+        public override void OnLoad(Harmony harmony)
+        {
+            base.OnLoad(harmony);
+            PUtil.InitLibrary();
+        }
+
+        [HarmonyPatch(typeof(SuitLocker), nameof(SuitLocker.UnequipFrom))]
+        private static class SuitLocker_UnequipFrom
+        {
+            private static void Prefix(SuitLocker __instance, Equipment equipment)
+            {
+                var resume = equipment.GetTargetGameObject()?.GetComponent<MinionResume>();
+                var durability = equipment.GetAssignable(Db.Get().AssignableSlots.Suit)?.GetComponent<Durability>();
+                if (durability != null && durability.IsTrueWornOut(resume))
+                {
+                    var lockerStorage = __instance.GetComponent<Storage>();
+                    var suitStorage = durability.GetComponent<Storage>();
+                    var suitTank = durability.GetComponent<SuitTank>();
+                    if (suitStorage != null && suitTank != null)
+                    {
+                        // повторяем, так как в той же маске могут быть два разных кислорода
+                        while (suitStorage.Transfer(lockerStorage, suitTank.elementTag, suitTank.capacity, false, true) > 0f) ;
+                    }
+                    var jetSuitTank = durability.GetComponent<JetSuitTank>();
+                    if (jetSuitTank)
+                    {
+                        lockerStorage.AddLiquid(SimHashes.Petroleum, jetSuitTank.amount, durability.GetComponent<PrimaryElement>().Temperature, byte.MaxValue, 0, false, true);
+                        jetSuitTank.amount = 0f;
+                    }
+                }
+            }
+        }
+    }
+}
