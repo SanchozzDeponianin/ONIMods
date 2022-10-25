a39099617ad6c7708fc3cc6d45810e748ae62362
SanchozzDeponianin <73459473+SanchozzDeponianin@users.noreply.github.com>
2022-10-25 11:46:14 +0600

    SmartLogicDoors: the description of the logical port, which can be seen in the automation overlay, is now updated according to the selected door state.

--- src/SmartLogicDoors/SmartLogicDoor.cs
+++ src/SmartLogicDoors/SmartLogicDoor.cs
@@ -1,9 +1,16 @@
-﻿using KSerialization;
+﻿using System;
+using KSerialization;
+using static STRINGS.BUILDINGS.PREFABS.DOOR;
+using static STRINGS.UI;
+using PeterHan.PLib.Core;
 
 namespace SmartLogicDoors
 {
     public class SmartLogicDoor : KMonoBehaviour
     {
+        private static string RedAutoDescription;
+        private static string GreenAutoDescription;
+
         [Serialize]
         public Door.ControlState RedState = Door.ControlState.Locked;
 
@@ -17,8 +24,61 @@ namespace SmartLogicDoors
 
         public bool IsLogicPortConnected => ports.IsPortConnected(Door.OPEN_CLOSE_PORT_ID);
 
+        protected override void OnPrefabInit()
+        {
+            base.OnPrefabInit();
+            if (string.IsNullOrEmpty(RedAutoDescription))
+            {
+                RedAutoDescription = $"{FormatAsAutomationState(LOGIC_PORTS.GATE_SINGLE_INPUT_ONE_INACTIVE, AutomationState.Standby)}: {CONTROL_STATE.AUTO.NAME}";
+                GreenAutoDescription = $"{FormatAsAutomationState(LOGIC_PORTS.GATE_SINGLE_INPUT_ONE_ACTIVE, AutomationState.Active)}: {CONTROL_STATE.AUTO.NAME}";
+            }
+        }
+
+        protected override void OnSpawn()
+        {
+            base.OnSpawn();
+            UpdateLogicPortDescription();
+        }
+
+        // обновляем описание логик. порта, которое отображается в оверлее и некоторых других местах
+        private void UpdateLogicPortDescription()
+        {
+            if (ports.inputPortInfo != null && ports.inputPortInfo.Length > 0)
+            {
+                int idx = Array.FindIndex(ports.inputPortInfo, port => port.id == Door.OPEN_CLOSE_PORT_ID);
+                if (idx != -1)
+                {
+                    switch (RedState)
+                    {
+                        case Door.ControlState.Opened:
+                            PUtil.LogWarning("Not Implemented");
+                            break;
+                        case Door.ControlState.Auto:
+                            ports.inputPortInfo[idx].inactiveDescription = RedAutoDescription;
+                            break;
+                        case Door.ControlState.Locked:
+                            ports.inputPortInfo[idx].inactiveDescription = LOGIC_OPEN_INACTIVE;
+                            break;
+                    }
+                    switch (GreenState)
+                    {
+                        case Door.ControlState.Opened:
+                            ports.inputPortInfo[idx].activeDescription = LOGIC_OPEN_ACTIVE;
+                            break;
+                        case Door.ControlState.Auto:
+                            ports.inputPortInfo[idx].activeDescription = GreenAutoDescription;
+                            break;
+                        case Door.ControlState.Locked:
+                            PUtil.LogWarning("Not Implemented");
+                            break;
+                    }
+                }
+            }
+        }
+
         public bool ApplyControlState()
         {
+            UpdateLogicPortDescription();
             if (IsLogicPortConnected)
             {
                 Trigger((int)GameHashes.LogicEvent, new LogicValueChanged()
