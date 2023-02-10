5d5b0777dffc33306c7711ab2b3dcfad488fcaae
SanchozzDeponianin <73459473+SanchozzDeponianin@users.noreply.github.com>
2023-02-10 11:51:11 +0600

    New mod "Don't Starve Mr. Puft" dynamically adjusts the rate of gas intake by creatures, such as Pufts and Slicksters, depending on their hunger.
    The hungrier the Puft is, the more gas he will inhale in one breath.
    Should help eliminate or partially reduce the consequences of the late game problem, when these Creatures eat much more rarely than expected due to lags, and therefore they are constantly starving.

---
+++ src/DontStarvePuft/DontStarvePuft.cs
@@ -0,0 +1,38 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using PeterHan.PLib.Detours;
+
+namespace DontStarvePuft
+{
+    internal sealed class DontStarvePuftPatches : KMod.UserMod2
+    {
+        [HarmonyPatch(typeof(GasAndLiquidConsumerMonitor.Instance), nameof(GasAndLiquidConsumerMonitor.Instance.Consume))]
+        private static class GasAndLiquidConsumerMonitor_Instance_Consume
+        {
+            private static readonly IDetouredField<GasAndLiquidConsumerMonitor.Instance, Element> TargetElement
+                = PDetours.DetourField<GasAndLiquidConsumerMonitor.Instance, Element>("targetElement");
+
+            private static void Prefix(GasAndLiquidConsumerMonitor.Instance __instance, ref float dt)
+            {
+                var targetElement = TargetElement.Get(__instance);
+                if (targetElement != null && __instance.def.diet != null)
+                {
+                    var info = __instance.def.diet.GetDietInfo(targetElement.tag);
+                    if (info != null)
+                    {
+                        var calories = Db.Get().Amounts.Calories.Lookup(__instance.gameObject);
+                        if (calories != null)
+                        {
+                            float factor01 = (1f - CreatureCalorieMonitor.Instance.HUNGRY_RATIO) + calories.value / calories.GetMax();
+                            factor01 = Mathf.Pow(factor01, 0.1f);
+                            float required_mass = info.ConvertCaloriesToConsumptionMass(-calories.GetDelta() * Constants.SECONDS_PER_CYCLE);
+                            required_mass = Mathf.Lerp(required_mass, 0, factor01);
+                            required_mass = Mathf.Max(__instance.def.consumptionRate, required_mass);
+                            dt *= required_mass / __instance.def.consumptionRate;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
