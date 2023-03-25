1cbaa2de171f004945df1bba27d15cc9b9f17131
SanchozzDeponianin <73459473+SanchozzDeponianin@users.noreply.github.com>
2023-03-26 00:45:33 +0600

    WrangleCarry: revised approach.
    now direct forced brain update instead of tag manipulation.
    added animation of the bag when the dupe is carrying an animal.

--- src/WrangleCarry/WrangleCarryPatches.cs
+++ src/WrangleCarry/WrangleCarryPatches.cs
@@ -1,4 +1,6 @@
-﻿using Database;
+﻿using System.Collections.Generic;
+using System.Reflection;
+using Database;
 using HarmonyLib;
 
 namespace WrangleCarry
@@ -18,10 +20,34 @@ namespace WrangleCarry
             }
         }
 
-        // обычно после поимки жеготного есть лаг до появления задачи переноса
-        // изза чего жеготновод убегает делать другие дела
-        // делаем жеготное доступным для переноса и роняем его на пол - сразу, в обход ИИ жеготных
-        // а также добавляем жеготноводу эмоцию после поимки
+        // ИИ жеготных как исвестно - подлагивает
+        // например, есть лаги между:
+        // а) жеготновод начал связывать жеготное, и жеготное поняло что его связывают и встало на месте
+        // б) жеготновод закончил связывать жеготное, и жеготное поняло что его связали, и стало доступным для переноса
+        // изза б) жеготновод убегает делать другие дела оставляя жеготное на полу
+        // поэтому принудительно вправляем мозги жеготному в начале и конце связывания
+        // todo: посмотреть что там с ловушками
+        [HarmonyPatch]
+        private static class Capturable_OnWork
+        {
+            private static IEnumerable<MethodBase> TargetMethods()
+            {
+                const BindingFlags flags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic;
+                yield return typeof(Capturable).GetMethod("OnStartWork", flags);
+                yield return typeof(Capturable).GetMethod("OnStopWork", flags);
+            }
+            private static void Postfix(Capturable __instance)
+            {
+                if (__instance.TryGetComponent<CreatureBrain>(out var brain) && brain.IsRunning())
+                    brain.UpdateBrain();
+            }
+        }
+
+        // добавляем жеготноводу эмоцию после поимки, если жеготное неподнимаемо прямо сейчас
+        // например зарытые хачи и ройки, или попотолкуходячие белочки и барашки
+        // с одной стороны это даст время жеготному упасть напол
+        // с другой - задачу переноса могут перетянуть на себя другие дупели
+        // в любом случае это выглядит запавно
         [HarmonyPatch(typeof(Capturable), "OnCompleteWork")]
         private static class Capturable_OnCompleteWork
         {
@@ -29,12 +55,44 @@ namespace WrangleCarry
             private static readonly HashedString[] anims = { "rage_pre", "rage_loop", "rage_loop", "rage_loop", "rage_pst" };
             private static void Postfix(Capturable __instance, Worker worker)
             {
-                __instance.AddTag(GameTags.Creatures.Deliverable);
-                __instance.GetSMI<BaggedStates.Instance>()?.UpdateFaller(true);
                 if (__instance.TryGetComponent<Pickupable>(out var pickupable) && pickupable.IsReachable())
                     return;
                 if (worker != null && worker.TryGetComponent<ChoreProvider>(out var provider))
                     new EmoteChore(provider, Db.Get().ChoreTypes.EmoteHighPriority, kanim, anims);
+            }
+        }
+
+        // косметика
+        // когда дупель несет жеготное, добавляем ему анимацию мешка за спиной
+        [HarmonyPatch(typeof(FetchAreaChore.States), nameof(FetchAreaChore.States.InitializeStates))]
+        private static class FetchAreaChore_States_InitializeStates
+        {
+            private const string chest = "snapTo_chest";
+            private static void SetSymbolOverride(FetchAreaChore.StatesInstance smi, bool enable)
+            {
+                var deliveryObject = smi.sm.deliveryObject.Get(smi);
+                if (deliveryObject != null && deliveryObject.HasTag(GameTags.Creatures.Deliverable)
+                    && smi.gameObject.TryGetComponent<KAnimControllerBase>(out var kbac)
+                    && smi.gameObject.TryGetComponent<SymbolOverrideController>(out var controller))
+                {
+                    if (enable)
+                    {
+                        var symbol = Assets.GetAnim("creature_sack_kanim").GetData().build.GetSymbol("object");
+                        controller.AddSymbolOverride(chest, symbol);
+                        kbac.SetSymbolVisiblity(chest, true);
+                    }
+                    else
+                    {
+                        kbac.SetSymbolVisiblity(chest, false);
+                        controller.RemoveSymbolOverride(chest);
+                    }
+                }
+            }
+            private static void Postfix(FetchAreaChore.States __instance)
+            {
+                __instance.delivering.movetostorage
+                    .Enter(smi => SetSymbolOverride(smi, true))
+                    .Exit(smi => SetSymbolOverride(smi, false));
             }
         }
     }
