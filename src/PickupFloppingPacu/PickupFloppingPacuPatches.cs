39b93e1a11d243f3f1adc9aabdcfa27f24a65579
SanchozzDeponianin <73459473+SanchozzDeponianin@users.noreply.github.com>
2023-06-30 21:54:29 +0600

    new mod PickupFloppingPacu allows Duplicants to pick up flopping Pacu

---
+++ src/PickupFloppingPacu/PickupFloppingPacuPatches.cs
@@ -0,0 +1,38 @@
+﻿using HarmonyLib;
+using SanchozzONIMods.Lib;
+
+namespace PickupFloppingPacu
+{
+    internal sealed class PickupFloppingPacuPatches : KMod.UserMod2
+    {
+        public override void OnLoad(Harmony harmony)
+        {
+            Utils.LogModVersion();
+            base.OnLoad(harmony);
+        }
+
+        [HarmonyPatch(typeof(FlopStates), nameof(FlopStates.InitializeStates))]
+        private static class FlopStates_InitializeStates
+        {
+            private static void Postfix(FlopStates __instance)
+            {
+                __instance.root
+                    .ToggleTag(GameTags.Creatures.Deliverable)
+                    .EventHandler(GameHashes.OnStore, ScheduleUpdateBrain);
+            }
+        }
+
+        private static void ScheduleUpdateBrain(StateMachine.Instance smi)
+        {
+            if (smi.gameObject.TryGetComponent<CreatureBrain>(out var brain))
+                GameScheduler.Instance.ScheduleNextFrame(null, ForceUpdateBrain, brain);
+        }
+
+        private static void ForceUpdateBrain(object data)
+        {
+            var brain = data as CreatureBrain;
+            if (brain != null && brain.IsRunning())
+                brain.UpdateBrain();
+        }
+    }
+}
