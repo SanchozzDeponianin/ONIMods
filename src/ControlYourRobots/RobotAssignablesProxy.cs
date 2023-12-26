821a3fc5670625f558e73e2b5b7054a9c5f66ccf
SanchozzDeponianin <73459473+SanchozzDeponianin@users.noreply.github.com>
2023-12-26 14:40:47 +0600

    ControlYourRobots: fixed a race condition that causes excessive creation of proxy objects when loading a save file containing many robots and saved before this feature was activated

--- src/ControlYourRobots/RobotAssignablesProxy.cs
+++ src/ControlYourRobots/RobotAssignablesProxy.cs
@@ -8,24 +8,9 @@
     public class RobotAssignablesProxy : MinionAssignablesProxy
     {
         private static readonly handler OnQueueDestroyObjectDelegate = new handler((cmp, data) => cmp.OnQueueDestroyObject(data));
-
         public static Components.Cmps<RobotAssignablesProxy> Cmps = new Components.Cmps<RobotAssignablesProxy>();
-
-        public static int GetRobotProxyID(Tag prefabID)
-        {
-            foreach (var proxi in Cmps.Items)
-            {
-                if (!proxi.IsNullOrDestroyed() && proxi.PrefabID == prefabID)
-                    return proxi.TargetInstanceID;
-            }
-            var go = GameUtil.KInstantiate(Assets.GetPrefab(RobotAssignablesProxyConfig.ID), Grid.SceneLayer.NoLayer);
-            var identity = go.GetComponent<RobotIdentity>();
-            identity.PrefabID = prefabID;
-            var proxy = go.GetComponent<RobotAssignablesProxy>();
-            proxy.SetTarget(identity, go);
-            go.SetActive(true);
-            return proxy.TargetInstanceID;
-        }
+        private static object @lock = new object();
+        SchedulerHandle handle;
 
         public Tag PrefabID
         {
@@ -46,14 +31,80 @@
         protected override void OnSpawn()
         {
             RestoreTargetFromInstanceID();
-            Cmps.Add(this);
+            TryAdd(this);
+            handle = GameScheduler.Instance.Schedule("cleanup", 2 * UpdateManager.SecondsPerSimTick, CleanupLimbo);
         }
 
-        protected override void OnCleanUp() { }
+        private void CleanupLimbo(object _)
+        {
+            // остаться должен только один (с)
+            foreach (var item in Cmps.Items)
+                if (item == this)
+                    return;
+            Util.KDestroyGameObject(gameObject);
+        }
+
+        protected override void OnCleanUp()
+        {
+            handle.ClearScheduler();
+        }
 
         private void OnQueueDestroyObject(object data)
         {
             Cmps.Remove(this);
+        }
+
+        private static bool TryGet(Tag prefabID, out RobotAssignablesProxy proxy)
+        {
+            lock (@lock)
+            {
+                foreach (var item in Cmps.Items)
+                {
+                    if (!item.IsNullOrDestroyed() && item.PrefabID == prefabID)
+                    {
+                        proxy = item;
+                        return true;
+                    }
+                }
+                proxy = null;
+                return false;
+            }
+        }
+
+        private static RobotAssignablesProxy TryAdd(RobotAssignablesProxy proxy)
+        {
+            var tag = proxy.PrefabID;
+            lock (@lock)
+            {
+                foreach (var item in Cmps.Items)
+                {
+                    if (!item.IsNullOrDestroyed() && item.PrefabID == tag)
+                    {
+                        return item;
+                    }
+                }
+                Cmps.Add(proxy);
+                return proxy;
+            }
+        }
+
+        private static RobotAssignablesProxy Instantiate(Tag prefabID)
+        {
+            var go = GameUtil.KInstantiate(Assets.GetPrefab(RobotAssignablesProxyConfig.ID), Grid.SceneLayer.NoLayer);
+            var identity = go.GetComponent<RobotIdentity>();
+            identity.PrefabID = prefabID;
+            var proxy = go.GetComponent<RobotAssignablesProxy>();
+            proxy.SetTarget(identity, go);
+            go.SetActive(true);
+            return proxy;
+        }
+
+        public static int GetRobotProxyID(Tag prefabID)
+        {
+            if (TryGet(prefabID, out var proxy))
+                return proxy.TargetInstanceID;
+            else
+                return TryAdd(Instantiate(prefabID)).TargetInstanceID;
         }
     }
 }
