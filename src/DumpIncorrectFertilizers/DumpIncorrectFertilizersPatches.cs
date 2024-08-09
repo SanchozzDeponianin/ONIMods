2d046cca8430abdf73a3cf397332cc78b5c8075e
SanchozzDeponianin <73459473+SanchozzDeponianin@users.noreply.github.com>
2024-08-09 23:22:16 +0500

    new mod DumpIncorrectFertilizers
    Now an errand to empty the farms storage of the wrong liquid is created automatically.

---
+++ src/DumpIncorrectFertilizers/DumpIncorrectFertilizersPatches.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using HarmonyLib;
+using UnityEngine;
+using SanchozzONIMods.Lib;
+
+namespace DumpIncorrectFertilizers
+{
+    internal sealed class DumpIncorrectFertilizersPatches : KMod.UserMod2
+    {
+        public override void OnLoad(Harmony harmony)
+        {
+            if (Utils.LogModVersion()) return;
+            base.OnLoad(harmony);
+        }
+
+        private static Chore CreateDumpChore(GameObject storage)
+        {
+            var workable = storage.AddOrGet<DumpIncorrectFertilizersWorkable>();
+            return new WorkChore<DumpIncorrectFertilizersWorkable>(Db.Get().ChoreTypes.EmptyStorage, workable, only_when_operational: false);
+        }
+
+        [HarmonyPatch(typeof(FertilizationMonitor), nameof(FertilizationMonitor.InitializeStates))]
+        private static class FertilizationMonitor_InitializeStates
+        {
+            private static void Postfix(FertilizationMonitor __instance)
+            {
+                __instance.replanted.starved.wrongFert
+                    .ToggleStatusItem(Db.Get().BuildingStatusItems.AwaitingEmptyBuilding)
+                    .ToggleRecurringChore(smi => CreateDumpChore(smi.sm.fertilizerStorage.Get(smi)), smi => smi.sm.fertilizerStorage.Get(smi) != null);
+            }
+        }
+
+        [HarmonyPatch(typeof(IrrigationMonitor), nameof(IrrigationMonitor.InitializeStates))]
+        private static class IrrigationMonitor_InitializeStates
+        {
+            private static void Postfix(IrrigationMonitor __instance)
+            {
+                __instance.replanted.starved.wrongLiquid
+                    .ToggleStatusItem(Db.Get().BuildingStatusItems.AwaitingEmptyBuilding)
+                    .ToggleRecurringChore(smi => CreateDumpChore(smi.sm.resourceStorage.Get(smi)), smi => smi.sm.resourceStorage.Get(smi) != null);
+            }
+        }
+
+        // чтобы вытащенное выпадало со стороны работничка
+        [HarmonyPatch(typeof(IrrigationMonitor.Instance), nameof(IrrigationMonitor.Instance.DumpIncorrectFertilizers))]
+        [HarmonyPatch(new Type[] { typeof(Storage), typeof(PlantElementAbsorber.ConsumeInfo[]), typeof(bool) })]
+        private static class IrrigationMonitor_Instance_DumpIncorrectFertilizers
+        {
+            /*
+                storage.Drop(gameObject, true)
+            +++ .Trigger((int)GameHashes.WorkableEntombOffset, workable.worker);
+            */
+            private static GameObject TriggerOffset(GameObject go, Storage storage)
+            {
+                if (go != null && storage != null && storage.TryGetComponent(out DumpIncorrectFertilizersWorkable workable) && workable.worker != null)
+                    go.Trigger((int)GameHashes.WorkableEntombOffset, workable.worker);
+                return go;
+            }
+
+            private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, MethodBase original)
+            {
+                return TranspilerUtils.Wrap(instructions, original, transpiler);
+            }
+            private static bool transpiler(List<CodeInstruction> instructions, MethodBase original)
+            {
+                var drop = typeof(Storage).GetMethod(nameof(Storage.Drop), new Type[] { typeof(GameObject), typeof(bool) });
+                var storage = original.GetParameters().FirstOrDefault(p => p.ParameterType == typeof(Storage));
+                var trigger = typeof(IrrigationMonitor_Instance_DumpIncorrectFertilizers)
+                    .GetMethod(nameof(TriggerOffset), BindingFlags.NonPublic | BindingFlags.Static);
+                if (drop != null && storage != null && trigger != null)
+                {
+                    int i = instructions.FindIndex(inst => inst.Calls(drop));
+                    if (i != -1)
+                    {
+                        instructions.Insert(++i, TranspilerUtils.GetLoadArgInstruction(storage));
+                        instructions.Insert(++i, new CodeInstruction(OpCodes.Call, trigger));
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+    }
+}
---
+++ src/DumpIncorrectFertilizers/DumpIncorrectFertilizersWorkable.cs
@@ -0,0 +1,31 @@
+﻿namespace DumpIncorrectFertilizers
+{
+    [SkipSaveFileSerialization]
+    public class DumpIncorrectFertilizersWorkable : Workable
+    {
+#pragma warning disable CS0649
+        [MyCmpReq]
+        PlantablePlot plot;
+
+        [MyCmpReq]
+        Storage storage;
+#pragma warning restore CS0649
+
+        protected override void OnPrefabInit()
+        {
+            base.OnPrefabInit();
+            SetOffsetTable(OffsetGroups.InvertedStandardTableWithCorners);
+            ConfigureMultitoolContext("build", EffectConfigs.BuildSplashId);
+            workerStatusItem = Db.Get().DuplicantStatusItems.Emptying;
+            synchronizeAnims = false;
+            faceTargetWhenWorking = true;
+            SetWorkTime(1f);
+        }
+
+        protected override void OnCompleteWork(Worker worker)
+        {
+            IrrigationMonitor.Instance.DumpIncorrectFertilizers(storage, plot.Occupant);
+            base.OnCompleteWork(worker);
+        }
+    }
+}
