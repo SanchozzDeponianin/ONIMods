e65250fdadad3cf8ccb5631f85b8f466e078f8c8
SanchozzDeponianin <73459473+SanchozzDeponianin@users.noreply.github.com>
2024-12-01 01:07:59 +0500

    LargeTelescope: fixed to work with the new U54 beta version.
    a significant simplification, since Klei refactored GasProviders and fixed exploits.

--- src/LargeTelescope/LargeTelescopePatches.cs
+++ src/LargeTelescope/LargeTelescopePatches.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
 using TUNING;
 using UnityEngine;
@@ -14,21 +13,19 @@ namespace LargeTelescope
 {
     internal sealed class LargeTelescopePatches : KMod.UserMod2
     {
-        private static LargeTelescopePatches @this;
         public override void OnLoad(Harmony harmony)
         {
             if (Utils.LogModVersion()) return;
-            @this = this;
             LargeTelescopeOptions.Reload();
             new PPatchManager(harmony).RegisterPatchClass(typeof(LargeTelescopePatches));
             new POptions().RegisterOptions(this, typeof(LargeTelescopeOptions));
         }
 
         [PLibMethod(RunAt.BeforeDbInit)]
-        private static void Localize()
+        private static void BeforeDbInit(Harmony harmony)
         {
             Utils.InitLocalization(typeof(STRINGS));
-            @this.PatchLater();
+            harmony.PatchAll();
         }
 
         // тюнинг искричества и стоимости
@@ -41,8 +38,8 @@ namespace LargeTelescope
                 __result.EnergyConsumptionWhenActive = BUILDINGS.ENERGY_CONSUMPTION_WHEN_ACTIVE.TIER4;
                 if (LargeTelescopeOptions.Instance.add_glass)
                 {
-                    __result.MaterialCategory = __result.MaterialCategory.AddItem(MATERIALS.GLASSES[0]).ToArray();
-                    __result.Mass = __result.Mass.AddItem(BUILDINGS.CONSTRUCTION_MASS_KG.TIER3[0]).ToArray();
+                    __result.MaterialCategory = __result.MaterialCategory.Append(MATERIALS.GLASSES[0]);
+                    __result.Mass = __result.Mass.Append(BUILDINGS.CONSTRUCTION_MASS_KG.TIER3[0]);
                 }
             }
         }
@@ -55,13 +52,13 @@ namespace LargeTelescope
             {
                 if (LargeTelescopeOptions.Instance.add_glass)
                 {
-                    __result.MaterialCategory = __result.MaterialCategory.AddItem(MATERIALS.GLASSES[0]).ToArray();
-                    __result.Mass = __result.Mass.AddItem(BUILDINGS.CONSTRUCTION_MASS_KG.TIER3[0]).ToArray();
+                    __result.MaterialCategory = __result.MaterialCategory.Append(MATERIALS.GLASSES[0]);
+                    __result.Mass = __result.Mass.Append(BUILDINGS.CONSTRUCTION_MASS_KG.TIER3[0]);
                 }
             }
         }
 
-        // тюнинг стройки внутри ракет, радиуса и скорости скана. исправленный газпрофидер
+        // тюнинг стройки внутри ракет, радиуса скана.
         [HarmonyPatch(typeof(ClusterTelescopeEnclosedConfig), nameof(ClusterTelescopeEnclosedConfig.ConfigureBuildingTemplate))]
         private static class ClusterTelescopeEnclosedConfig_ConfigureBuildingTemplate
         {
@@ -71,18 +68,6 @@ namespace LargeTelescope
                 if (LargeTelescopeOptions.Instance.prohibit_inside_rocket)
                     go.AddOrGet<KPrefabID>().AddTag(GameTags.NotRocketInteriorBuilding);
                 go.AddOrGetDef<ClusterTelescope.Def>().analyzeClusterRadius = LargeTelescopeOptions.Instance.analyze_cluster_radius;
-                go.AddOrGet<TelescopeGasProvider>().efficiencyMultiplier = 1f + (LargeTelescopeOptions.Instance.efficiency_multiplier / 100f);
-            }
-        }
-
-        // исправленный газпрофидер для ванильного телескопа
-        [HarmonyPatch(typeof(TelescopeConfig), nameof(TelescopeConfig.ConfigureBuildingTemplate))]
-        private static class TelescopeConfig_ConfigureBuildingTemplate
-        {
-            private static bool Prepare() => DlcManager.IsPureVanilla();
-            private static void Postfix(GameObject go)
-            {
-                go.AddOrGet<TelescopeGasProvider>().efficiencyMultiplier = 1f;
             }
         }
 
@@ -110,102 +95,39 @@ namespace LargeTelescope
         }
 
         // убираем требование наличия трубы
-        [HarmonyPatch(typeof(BuildingConfigManager), nameof(BuildingConfigManager.ConfigurePost))]
-        private static class BuildingConfigManager_ConfigurePost
+        [PLibMethod(RunAt.BeforeDbPostProcess)]
+        private static void BeforeDbPostProcess()
         {
-            private static bool Prepare() => LargeTelescopeOptions.Instance.not_require_gas_pipe;
-            private static void Postfix()
+            if (LargeTelescopeOptions.Instance.not_require_gas_pipe)
             {
                 var id = DlcManager.IsExpansion1Active() ? ClusterTelescopeEnclosedConfig.ID : TelescopeConfig.ID;
                 Assets.GetBuildingDef(id).BuildingComplete.GetComponent<RequireInputs>().SetRequirements(true, false);
             }
         }
 
-        // отключаем манипуляции клеев с GasProvider
-        [HarmonyPatch]
-        private static class Telescopes_OnWorkableEvent
-        {
-            private static IEnumerable<MethodBase> TargetMethods()
-            {
-                const string method = "OnWorkableEvent";
-                var args = new Type[] { typeof(Workable), typeof(Workable.WorkableEvent) };
-                if (DlcManager.IsExpansion1Active())
-                {
-                    yield return typeof(ClusterTelescope.ClusterTelescopeWorkable).GetMethodSafe(method, false, args);
-                    yield return typeof(ClusterTelescope.ClusterTelescopeIdentifyMeteorWorkable).GetMethodSafe(method, false, args);
-                }
-                else
-                {
-                    yield return typeof(Telescope).GetMethodSafe(method, false, args);
-                }
-            }
-
-            private static void StubSetGasProvider(OxygenBreather breather, OxygenBreather.IGasProvider gas_provider) { }
-
-            private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, MethodBase original)
-            {
-                return TranspilerUtils.Wrap(instructions, original, transpiler);
-            }
-
-            private static bool transpiler(List<CodeInstruction> instructions)
-            {
-                var SetGasProvider = typeof(OxygenBreather).GetMethodSafe(nameof(OxygenBreather.SetGasProvider), false, typeof(OxygenBreather.IGasProvider));
-                var Stub = typeof(Telescopes_OnWorkableEvent).GetMethodSafe(nameof(StubSetGasProvider), true, PPatchTools.AnyArguments);
-                if (SetGasProvider != null && Stub != null)
-                {
-                    instructions = PPatchTools.ReplaceMethodCallSafe(instructions, SetGasProvider, Stub).ToList();
-                    return true;
-                }
-                return false;
-            }
-
-            // и добавляем свой исправленный газпрофидер
-            private static void Postfix(Workable workable, Workable.WorkableEvent ev)
-            {
-                if (workable != null && workable.TryGetComponent<TelescopeGasProvider>(out var gasProvider))
-                {
-                    if (ev == Workable.WorkableEvent.WorkStarted && !gasProvider.IsEmpty)
-                    {
-                        gasProvider.OverrideGasProvider(workable.worker);
-                    }
-                    else if (ev == Workable.WorkableEvent.WorkStopped)
-                    {
-                        gasProvider.RestoreGasProvider();
-                    }
-                }
-            }
-        }
-
         // ускоряем скорость работы
-        // переключаем газпрофидеры если в процессе работы закончился/снова появился кислород
         [HarmonyPatch]
         private static class Telescopes_OnWorkTick
         {
+            private static float efficiencyMultiplier = 1f;
+            private static bool Prepare()
+            {
+                efficiencyMultiplier = 1f + (LargeTelescopeOptions.Instance.efficiency_multiplier / 100f);
+                return DlcManager.IsExpansion1Active();
+            }
+
             private static IEnumerable<MethodBase> TargetMethods()
             {
                 const string method = "OnWorkTick";
                 var args = new Type[] { typeof(WorkerBase), typeof(float) };
-                if (DlcManager.IsExpansion1Active())
-                {
-                    yield return typeof(ClusterTelescope.ClusterTelescopeWorkable).GetMethodSafe(method, false, args);
-                    yield return typeof(ClusterTelescope.ClusterTelescopeIdentifyMeteorWorkable).GetMethodSafe(method, false, args);
-                }
-                else
-                {
-                    yield return typeof(Telescope).GetMethodSafe(method, false, args);
-                }
+                yield return typeof(ClusterTelescope.ClusterTelescopeWorkable).GetMethodSafe(method, false, args);
+                yield return typeof(ClusterTelescope.ClusterTelescopeIdentifyMeteorWorkable).GetMethodSafe(method, false, args);
             }
 
-            private static void Prefix(Workable __instance, WorkerBase worker, ref float dt)
+            private static void Prefix(ClusterTelescope.Instance ___m_telescope, ref float dt)
             {
-                if (__instance != null && __instance.TryGetComponent<TelescopeGasProvider>(out var gasProvider))
-                {
-                    if (gasProvider.IsEmpty)
-                        gasProvider.RestoreGasProvider();
-                    else if (!gasProvider.HasBreather)
-                        gasProvider.OverrideGasProvider(worker);
-                    dt *= gasProvider.efficiencyMultiplier;
-                }
+                if (___m_telescope.providesOxygen)
+                    dt *= efficiencyMultiplier;
             }
         }
     }
