2d22749eee0ebb94b2a73f5e1311a1cd4468c7ce
SanchozzDeponianin <73459473+SanchozzDeponianin@users.noreply.github.com>
2025-07-03 15:57:19 +0500

    CrabsProfit: shells now change their visuals depending on their mass.

--- src/CrabsProfit/CrabFreshWaterShellConfig.cs
+++ src/CrabsProfit/CrabFreshWaterShellConfig.cs
@@ -24,7 +24,7 @@ namespace CrabsProfit
                 desc: CRAB_SHELL.VARIANT_FRESH_WATER.DESC,
                 mass: 1f,
                 unitMass: false,
-                anim: Assets.GetAnim("fresh_crabshells_large_kanim"),
+                anim: Assets.GetAnim("fresh_crabshell_kanim"),
                 initialAnim: "object",
                 sceneLayer: Grid.SceneLayer.Front,
                 collisionShape: EntityTemplates.CollisionShape.RECTANGLE,
@@ -36,12 +36,55 @@ namespace CrabsProfit
                 additionalTags: new List<Tag> { GameTags.Organics, GameTags.MoltShell });
             go.AddOrGet<EntitySplitter>();
             go.AddOrGet<SimpleMassStatusItem>();
+            // размер шкорлупы:
+            var id = (OreSizeVisualizerComponents.TiersSetType)Hash.SDBMLower(ID);
+            go.AddOrGet<EntitySizeVisualizer>().TierSetType = id;
+            var mass_tiers = new OreSizeVisualizerComponents.MassTier[]
+            {
+                new OreSizeVisualizerComponents.MassTier
+                {
+                    animName = "idle1",
+                    massRequired = 1.5f * ModOptions.Instance.BabyShellMass,
+                    colliderRadius = 0.15f
+                },
+                new OreSizeVisualizerComponents.MassTier
+                {
+                    animName = "idle2",
+                    massRequired = 1.5f * ModOptions.Instance.AdultShellMass,
+                    colliderRadius = 0.2f
+                },
+                new OreSizeVisualizerComponents.MassTier
+                {
+                    animName = "idle3",
+                    massRequired = float.MaxValue,
+                    colliderRadius = 0.25f
+                }
+            };
+            OreSizeVisualizerComponents.TierSets[id] = mass_tiers;
             EntityTemplates.CreateAndRegisterCompostableFromPrefab(go);
             AddRecipe(ID, mass);
             return go;
         }
 
-        public void OnPrefabInit(GameObject inst) { }
+        public void OnPrefabInit(GameObject inst)
+        {
+            // скопипизжено из других шкорлуп, наверно для правильной загрузки сейфоф до У56
+            if (inst.TryGetComponent(out Compostable compostable))
+                compostable.OnDeserializeCb = (KMonoBehaviour kmb) =>
+            {
+                if (SaveLoader.Instance.GameInfo.IsVersionOlderThan(7, 36))
+                {
+                    if (kmb.TryGetComponent(out PrimaryElement pe))
+                    {
+                        pe.MassPerUnit = 1f;
+                        pe.Mass = pe.Units * ModOptions.Instance.AdultShellMass;
+                    }
+                    if (kmb.TryGetComponent(out KPrefabID kPrefabID))
+                        kPrefabID.RemoveTag(GameTags.IndustrialIngredient);
+                }
+            };
+        }
+
         public void OnSpawn(GameObject inst) { }
 
         internal static void AddRecipe(string shell_id, float shell_mass)
