2f4517e5e0276ee101990cc78fa8afe197999d36
SanchozzDeponianin <73459473+SanchozzDeponianin@users.noreply.github.com>
2025-11-17 18:02:01 +0500

    new mod Crabs flip Compost

---
+++ src/CrabsFlippCompost/FlippCompostMonitor.cs
@@ -0,0 +1,164 @@
+﻿using UnityEngine;
+using static STRINGS.DUPLICANTS.CHORES;
+
+namespace CrabsFlippCompost
+{
+    public class FlippCompostMonitor : GameStateMachine<FlippCompostMonitor, FlippCompostMonitor.Instance, IStateMachineTarget, FlippCompostMonitor.Def>
+    {
+        public static readonly Tag ID = new(nameof(FlippCompostMonitor));
+        public static readonly Tag COMPOST_ID = new(CompostConfig.ID);
+        public static readonly Tag BEHAVIOUR_TAG = new("WantsToFlippCompost");
+
+        public class Def : BaseDef
+        {
+            public int radius = 25;
+            public float minCooldown = 20f;
+            public float maxCooldown = 60f;
+            public CellOffset[] approachOffsets = new[] { new CellOffset(-1, 0), new CellOffset(2, 0) }; // справа и слева от компоста
+            private Navigator.Scanner<BuildingComplete> compostSeeker;
+            public Navigator.Scanner<BuildingComplete> CompostSeeker
+            {
+                get
+                {
+                    if (compostSeeker == null)
+                    {
+                        compostSeeker = new(radius, GameScenePartitioner.Instance.completeBuildings, IsCompostReady);
+                        compostSeeker.SetConstantOffsets(approachOffsets);
+                        compostSeeker.SetEarlyOutThreshold(8);
+                    }
+                    return compostSeeker;
+                }
+            }
+
+            private static bool IsCompostReady(BuildingComplete building)
+            {
+                bool result = building != null && building.prefabid.IsPrefabID(COMPOST_ID)
+                    && !building.prefabid.HasTag(GameTags.Creatures.ReservedByCreature)
+                    && building.TryGetComponent(out Compost compost) && compost.smi.IsInsideState(compost.smi.sm.inert)
+                    && building.TryGetComponent(out CompostWorkable workable) && workable.worker == null;
+                return result;
+            }
+        }
+
+        public new class Instance : GameInstance, IApproachableBehaviour, ICreatureMonitor
+        {
+            private KPrefabID kprefabid;
+            public Navigator navigator;
+            private Facing facing;
+            public GameObject target;
+            public Compost compost;
+            public CompostWorkable workable;
+            public int targetCell;
+
+            public Instance(IStateMachineTarget master, Def def) : base(master, def)
+            {
+                kprefabid = master.GetComponent<KPrefabID>();
+                navigator = master.GetComponent<Navigator>();
+                facing = master.GetComponent<Facing>();
+            }
+
+            public Tag Id => ID;
+            public bool IsValidTarget()
+            {
+                return !target.IsNullOrDestroyed() && navigator.GetNavigationCost(targetCell, GetApproachOffsets()) != -1 && workable.worker == null
+                    && (compost.smi.IsInsideState(compost.smi.sm.inert) || compost.smi.IsInsideState(Patches.Compost_States.waiting));
+            }
+            public GameObject GetTarget() => target;
+            public StatusItem GetApproachStatusItem() => smi.sm.TravelingToCompost;
+            public StatusItem GetBehaviourStatusItem() => smi.sm.Composting;
+            public CellOffset[] GetApproachOffsets() => def.approachOffsets;
+            public void OnArrive()
+            {
+                kprefabid.AddTag(GameTags.PerformingWorkRequest);
+                facing.Face(Grid.CellColumn(targetCell));
+                if (!target.IsNullOrDestroyed())
+                {
+                    bool fa = facing.GetFacing();
+                    var fx = FXHelpers.CreateEffect("flipp_compost_fx_kanim", target.transform.position + (fa ? Vector3.right : Vector3.zero),
+                        null, false, Grid.SceneLayer.BuildingFront, true);
+                    fx.initialAnim = fa ? "loop_r" : "loop_l";
+                    fx.destroyOnAnimComplete = true;
+                    fx.gameObject.SetActive(true);
+                }
+            }
+            public void OnSuccess()
+            {
+                if (!compost.IsNullOrDestroyed())
+                    compost.smi.GoTo(compost.smi.sm.composting);
+                target = null;
+                compost = null;
+                workable = null;
+                kprefabid.RemoveTag(GameTags.PerformingWorkRequest);
+            }
+            public void OnFailure() => kprefabid.RemoveTag(GameTags.PerformingWorkRequest);
+        }
+
+        private StatusItem TravelingToCompost;
+        private StatusItem Composting;
+
+        public State lookingForCompost;
+        public State satisfied;
+        public FloatParameter cooldown;
+
+        public override void InitializeStates(out BaseState default_state)
+        {
+            const string icon = "status_item_pending_compost";
+            TravelingToCompost = new(nameof(TravelingToCompost), FLIPCOMPOST.STATUS.text, FLIPCOMPOST.TOOLTIP.text,
+                icon, StatusItem.IconType.Custom, NotificationType.Neutral, false, OverlayModes.None.ID, showWorldIcon: false);
+            Composting = new(nameof(Composting), FLIPCOMPOST.NAME.text, FLIPCOMPOST.TOOLTIP.text,
+                icon, StatusItem.IconType.Custom, NotificationType.Neutral, false, OverlayModes.None.ID, showWorldIcon: false);
+
+            default_state = satisfied;
+            serializable = SerializeType.Never;
+            root.Enter(SetRandomCooldown);
+
+            lookingForCompost
+                .Enter(SetRandomCooldown)
+                .PreBrainUpdate(FindCompostTarget)
+                .ToggleBehaviour(BEHAVIOUR_TAG, smi => smi.IsValidTarget(), smi => smi.GoTo(satisfied));
+
+            satisfied
+                .ScheduleGoTo(smi => smi.sm.cooldown.Get(smi), lookingForCompost);
+        }
+
+        private static void SetMinCooldown(Instance smi)
+        {
+            smi.sm.cooldown.Set(smi.def.minCooldown, smi);
+        }
+        private static void SetRandomCooldown(Instance smi)
+        {
+            smi.sm.cooldown.Set(Random.Range(smi.def.minCooldown, smi.def.maxCooldown), smi);
+        }
+        private static void FindCompostTarget(Instance smi)
+        {
+            if (!smi.IsValidTarget())
+            {
+                var building = smi.def.CompostSeeker.Scan(Grid.PosToXY(smi.transform.GetPosition()), smi.navigator);
+                var go = building?.gameObject;
+                if (go != smi.target)
+                {
+                    if (go != null)
+                    {
+                        smi.target = go;
+                        go.TryGetComponent(out smi.compost);
+                        go.TryGetComponent(out smi.workable);
+                        smi.targetCell = Grid.PosToCell(smi.target);
+                    }
+                    else
+                    {
+                        smi.target = null;
+                        smi.compost = null;
+                        smi.workable = null;
+                        smi.targetCell = Grid.InvalidCell;
+                    }
+                    smi.Trigger((int)GameHashes.ApproachableTargetChanged, null);
+                }
+                if (smi.target == null)
+                {
+                    SetMinCooldown(smi);
+                    smi.GoTo(smi.sm.satisfied);
+                }
+            }
+        }
+    }
+}
---
+++ src/CrabsFlippCompost/Patches.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using HarmonyLib;
+using UnityEngine;
+using SanchozzONIMods.Lib;
+using PeterHan.PLib.Core;
+using PeterHan.PLib.PatchManager;
+
+namespace CrabsFlippCompost
+{
+    internal sealed class Patches : KMod.UserMod2
+    {
+        public override void OnLoad(Harmony harmony)
+        {
+            if (this.LogModVersion()) return;
+            new PPatchManager(harmony).RegisterPatchClass(typeof(Patches));
+            base.OnLoad(harmony);
+        }
+
+        [PLibMethod(RunAt.BeforeDbInit)]
+        private static void BeforeDbInit(Harmony harmony)
+        {
+            Utils.InitLocalization(typeof(STRINGS));
+            Utils.LoadEmbeddedAudioSheet("AudioSheets/SFXTags.csv");
+        }
+
+        // добавить крабам новое поведение
+        [HarmonyPatch(typeof(BaseCrabConfig), nameof(BaseCrabConfig.BaseCrab))]
+        private static class BaseCrabConfig_BaseCrab
+        {
+            internal static void Postfix(GameObject __result, bool is_baby)
+            {
+                if (!is_baby)
+                    __result.AddOrGetDef<FlippCompostMonitor.Def>();
+            }
+            // внедряем новое поведение
+            /*
+            ChoreTable.Builder chore_table = new ChoreTable.Builder().Add(new DeathStates.Def())
+                <блаблабла>
+                .Add(new CritterCondoStates.Def());
+        +++     .Add(new ApproachBehaviourStates.Def())
+                .Add(new CritterEmoteStates.Def());
+                .PopInterruptGroup()
+                .Add(new IdleStates.Def());
+            */
+            private static ChoreTable.Builder Inject(ChoreTable.Builder builder, bool is_baby)
+            {
+                return builder.Add(new ApproachBehaviourStates.Def(FlippCompostMonitor.ID, FlippCompostMonitor.BEHAVIOUR_TAG)
+                { preAnim = "slap_pre", loopAnim = "slap", pstAnim = "slap_pst" }, !is_baby);
+            }
+
+            internal static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, MethodBase original)
+            {
+                return instructions.Transpile(original, transpiler);
+            }
+
+            private static bool transpiler(ref List<CodeInstruction> instructions, MethodBase original)
+            {
+                var is_baby = original.GetParameters().FindFirst(p => p.ParameterType == typeof(bool) && p.Name == "is_baby");
+                var add = typeof(ChoreTable.Builder).GetMethodSafe(nameof(ChoreTable.Builder.Add), false,
+                    typeof(StateMachine.BaseDef), typeof(bool), typeof(int));
+                var emote = typeof(CritterEmoteStates.Def).GetConstructors()[0];
+                var inject = typeof(BaseCrabConfig_BaseCrab).GetMethodSafe(nameof(Inject), true, typeof(ChoreTable.Builder), typeof(bool));
+
+                if (add == null || emote == null || inject == null)
+                    return false;
+
+                int i = instructions.FindIndex(inst => inst.Is(OpCodes.Newobj, emote));
+                if (i == -1)
+                    return false;
+
+                int j = instructions.FindLastIndex(i, inst => inst.Calls(add));
+                if (j == -1)
+                    return false;
+
+                instructions.Insert(++j, is_baby.GetLoadArgInstruction());
+                instructions.Insert(++j, new CodeInstruction(OpCodes.Call, inject));
+                return true;
+            }
+        }
+
+        // новый статус в компосте, без чоры
+        [HarmonyPatch(typeof(Compost.States), nameof(Compost.States.InitializeStates))]
+        internal static class Compost_States
+        {
+            internal static Compost.States.State waiting;
+
+            private static void Postfix(Compost.States __instance)
+            {
+                waiting = __instance.CreateState("waiting_critter");
+
+                __instance.inert
+                    .TagTransition(GameTags.Creatures.ReservedByCreature, waiting, false);
+
+                waiting
+                    .EventTransition(GameHashes.OperationalChanged, __instance.disabled, smi => !smi.GetComponent<Operational>().IsOperational)
+                    .TagTransition(GameTags.Creatures.ReservedByCreature, __instance.inert, true)
+                    .PlayAnim("on")
+                    .ToggleStatusItem(Db.Get().BuildingStatusItems.AwaitingCompostFlip, null);
+            }
+        }
+    }
+}
